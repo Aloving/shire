2bb478298c2fbe716cb890fe0498cca4b896b2d9
Aloving <Ilegend2011@gmail.com>
2018-01-08 03:39:25 +0300

    BK-3 Имзменяет экспорт утилит

--- src/server/utils/index.js
+++ src/server/utils/index.js
@@ -1,8 +1,6 @@
 const validator = require('./validator');
 const mongoose = require('./mongoose');
 
-exports.mongoose = mongoose;
-exports.validator = validator;
 module.exports = {
   validator,
   mongoose,
